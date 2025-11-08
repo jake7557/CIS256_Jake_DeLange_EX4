0e04126d595ed909c30494df5c6a5e62fab70d24
jake7557 <jac2076337@maricopa.edu>
2025-11-07 20:08:36 -0700

    Code added

--- guess_the_word.py
+++ guess_the_word.py
@@ -1,10 +1,9 @@
-Python 3.13.2 (tags/v3.13.2:4f8bb39, Feb  4 2025, 15:23:48) [MSC v.1942 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> import random
-... 
-... # Random word list
-... word_list = ["giraffe", "elephant", "dinosaur", "ostrich", "whale", "lizard"]
-... # Randomly chosen word from list
+
+import random
+ 
+# Random word list
+word_list = ["giraffe", "elephant", "dinosaur", "ostrich", "whale", "lizard"]
+# Randomly chosen word from list
 
 print("Please try to guess what letters are in the random word.")
 
